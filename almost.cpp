e182704a14e4c3e53168cb8bdfadc5bc22846fc1
gheux <ialm0s9@outlook.com>
2024-06-07 23:18:21 +0800

    Modify almost.cpp

--- almost.cpp
+++ almost.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Moment have simply tax quite artist.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

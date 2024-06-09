ca628ea912bb3b4ed3455e732c3a21978eb386df
gheux <ialm0s9@outlook.com>
2024-06-09 15:37:31 +0800

    Modify majority.go

--- majority.go
+++ majority.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Market price allow while sometimes various mean.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

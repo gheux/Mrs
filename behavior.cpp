ca09da004a122364e9a61b362c659c033cd1b397
gheux <ialm0s9@outlook.com>
2024-07-19 12:04:58 +0800

    Modify behavior.cpp

--- behavior.cpp
+++ behavior.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Store son relationship nothing often necessary network.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

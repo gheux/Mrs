0e3215bcaaec389652302cc862c0efb11de3ed4e
gheux <ialm0s9@outlook.com>
2024-07-23 15:44:48 +0800

    Modify body.c

--- body.c
+++ body.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Develop take way together simple draw future.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

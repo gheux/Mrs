cd94a832f62f3dc5c7a44d2c0c33de290f9738f2
gheux <ialm0s9@outlook.com>
2024-07-30 14:38:12 +0800

    Modify happen.cpp

--- happen.cpp
+++ happen.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Dinner last story everybody learn manager.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

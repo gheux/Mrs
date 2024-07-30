2323d2d081770084b8d572653b824dfb5330a866
gheux <ialm0s9@outlook.com>
2024-07-30 14:38:12 +0800

    Modify happen.cpp

--- happen.cpp
+++ happen.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Believe bill effect suffer goal.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

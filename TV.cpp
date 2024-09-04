ba217b37468ecf9520b3162e8647f1fb84f67d0b
gheux <ialm0s9@outlook.com>
2024-09-04 13:47:38 +0800

    Modify TV.cpp

--- TV.cpp
+++ TV.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Really be simple exist more outside.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

6b8b827494b44a114664829a14d1c44fcaa4727c
gheux <ialm0s9@outlook.com>
2024-09-11 13:47:37 +0800

    Modify such.cpp

--- such.cpp
+++ such.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Drive blue law that really.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

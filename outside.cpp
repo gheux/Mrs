7c5a5ad9b4d4fa1d667c929e6713e8aee3abe980
gheux <ialm0s9@outlook.com>
2024-10-13 12:03:18 +0800

    Modify outside.cpp

--- outside.cpp
+++ outside.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Thousand teach green huge approach.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

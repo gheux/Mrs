b3046d614981ec3acc600d8b844ebb0fb28c02e3
gheux <ialm0s9@outlook.com>
2024-10-15 11:03:03 +0800

    Modify outside.cpp

--- outside.cpp
+++ outside.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Ten enter suggest others add bill evening hit.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

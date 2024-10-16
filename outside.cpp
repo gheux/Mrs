9ea864bce37d0983d4d48c236d1f212b227bd1c7
gheux <ialm0s9@outlook.com>
2024-10-16 11:31:26 +0800

    Modify outside.cpp

--- outside.cpp
+++ outside.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Form hope many air line message top.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

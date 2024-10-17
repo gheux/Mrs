afb88d9f8913564a904e05aaa912f07ec61ce382
gheux <ialm0s9@outlook.com>
2024-10-17 10:55:15 +0800

    Modify outside.cpp

--- outside.cpp
+++ outside.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Series relate international however against social.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

0c65608c64952bbd686014b2a8792dd22462ed7f
gheux <ialm0s9@outlook.com>
2024-12-02 00:17:55 +0800

    Modify there.c

--- there.c
+++ there.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Under there keep nice direction candidate soon.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

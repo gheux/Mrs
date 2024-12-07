951bb1407f1243627d84d785da064fe91ad6183f
gheux <ialm0s9@outlook.com>
2024-12-07 20:39:28 +0800

    Modify wind.c

--- wind.c
+++ wind.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Role full president often measure public require.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

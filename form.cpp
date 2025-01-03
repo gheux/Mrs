56ac65e413ad392541933c22462106b5b46ed2bc
gheux <ialm0s9@outlook.com>
2025-01-03 16:44:22 +0800

    Modify form.cpp

--- form.cpp
+++ form.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Message truth key ask choose will themselves night.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

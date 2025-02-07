8ed89777e369864afd938d04bedaa38b43f8b1da
gheux <ialm0s9@outlook.com>
2025-02-07 13:04:44 +0800

    Create tonight.c

--- tonight.c
+++ tonight.c
@@ -3,24 +3,3 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
-
-# Able toward suggest coach image traditional.
-#include <stdio.h>
-int main() {
-	printf("Hello, World!\n");
-	return 0;
-}
-
-# Certainly identify try ten same fly.
-#include <stdio.h>
-int main() {
-	printf("Hello, World!\n");
-	return 0;
-}
-
-# Eight rather drop.
-#include <stdio.h>
-int main() {
-	printf("Hello, World!\n");
-	return 0;
-}

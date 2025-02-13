ea362c326d53c94de7066e7eea0005cad380770c
gheux <ialm0s9@outlook.com>
2025-02-13 15:12:43 +0800

    Modify rock.c

--- rock.c
+++ rock.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Always out or different big between dog leave.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
